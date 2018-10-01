37fb917b566da90735f40a443b843ccfae0afb92
Arun Seehra <seehra.a@gmail.com>
2018-10-01 13:49:21 -0500

    Add basic data store

--- app.js
+++ app.js
@@ -1,8 +1,35 @@
-const state = {
-};
+'use strict';
 
 // State modification functions
+const Store = {
+  Item: {
+    nextID: (function () {
+      let id = 0;
+      return () => id++;
+    })(),
+
+    init() {
+      this.id = this.nextID();
+      return this;
+    }
+  },
+
+  init(numRows, numCols) {
+    this.table = Array.from({length: numRows}, () => {
+      return Array.from({length: numCols}, () => {
+        return Object.create(this.Item).init();
+      });
+    });
+
+    return this;
+  }
+};
 
 // Render functions
+const render = (function () {
+}());
 
 // Event Listeners
+$(() => {
+  const store = Object.create(Store).init(3, 3);
+});
